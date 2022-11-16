fc17f32eefa4fd8cf77ade3167917413f356d017
Oskar Ohlenmacher <katzentante@protonmail.com>
2022-11-16 22:24:46 +0100

    Added converting file partially to identifiers

---
+++ src/lexer.rs
@@ -0,0 +1,127 @@
+use std::{
+    fs::File,
+    io::{self, BufRead},
+    path::Path,
+};
+
+#[derive(Debug)]
+pub enum Indentifier {
+    Class,
+    Interface,
+    Enum,
+    StartObject,
+    EndObject,
+    StartMethod,
+    EndMethod,
+
+    Public,
+    Protected,
+    Private,
+    Abstract,
+    Static,
+
+    Type(String),
+    Variable(String),
+    Name(String),
+
+    InheritesLeft,
+    InheritesRight,
+
+    Startyuml,
+    Enduml,
+}
+
+// TODO merge lists ot one
+/* pub fn get_identifiers<'a>(filename: &'a str) -> std::io::Result<Vec<Vec<Indentifier>>> {
+    let path = Path::new(filename);
+    let file = File::open(path)?;
+    let list: Vec<Vec<Indentifier>> = io::BufReader::new(file)
+        .lines()
+        .filter(|x| x.is_ok())
+        .map(|x| x.unwrap())
+        .map(|line| parse_line(line))
+        .collect();
+    let mut out = Vec::new();
+    list.iter().map(|l| out.extend(l));
+    Ok(list)
+}
+*/
+// TODO remove pub
+pub fn get_identifiers(line: String) -> Vec<Indentifier> {
+    // ignore comments
+    match line.chars().nth(0) {
+        Some('\'') => return Vec::new(),
+        _ => (),
+    };
+    match line.as_str() {
+        "@startyuml" => return vec![Indentifier::Startyuml],
+        "@endyuml" => return vec![Indentifier::Enduml],
+        _ => (),
+    };
+
+    let mut out = Vec::new();
+    let mut object_started = false;
+    let mut second_object_started = false;
+    let mut ident = String::new();
+    let chars = line.chars();
+    for char in chars {
+        match char {
+            ' ' => match ident.as_str() {
+                "class" => out.push(Indentifier::Class),
+                "interface" => out.push(Indentifier::Interface),
+                _ => continue,
+            },
+            '+' => out.push(Indentifier::Public),
+            '#' => out.push(Indentifier::Protected),
+            '-' => out.push(Indentifier::Private),
+            ':' => {
+                if ident.trim().len() > 0 {
+                    out.push(Indentifier::Variable(ident.clone()))
+                }
+            }
+            '\n' => {
+                if ident.len() > 0 {
+                    out.push(Indentifier::Type(ident.clone()));
+                }
+            }
+            '{' => {
+                if !object_started {
+                    out.push(Indentifier::Name(ident.clone()));
+                    out.push(Indentifier::StartObject);
+                    object_started = true;
+                } else {
+                    second_object_started = true;
+                }
+            }
+            '(' => {
+                out.push(Indentifier::Name(ident.clone()));
+                out.push(Indentifier::StartMethod)
+            }
+            '}' => {
+                if second_object_started {
+                    match ident.as_str() {
+                        "abstract" => out.push(Indentifier::Abstract),
+                        "static" => out.push(Indentifier::Static),
+                        _ => (),
+                    }
+                    second_object_started = false;
+                } else {
+                    out.push(Indentifier::EndObject);
+                    object_started = false;
+                }
+            }
+            ')' => {
+                out.push(Indentifier::Type(ident.clone()));
+                out.push(Indentifier::EndMethod)
+            }
+            ',' => out.push(Indentifier::Type(ident.clone())),
+            _ => {
+                ident.push(char.clone());
+                continue;
+            }
+        }
+        ident.clear();
+    }
+
+    out
+}
