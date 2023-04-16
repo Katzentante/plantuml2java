ced43631b27d8183077d2647e33cb83aff7164fe
Oskar Ohlenmacher <katzentante@protonmail.com>
2023-04-16 15:22:11 +0200

    Added copyright notices

--- src/generate.rs
+++ src/generate.rs
@@ -1,3 +1,9 @@
+// Copyright (c) 2023, Oskar Ohlenmacher
+// All rights reserved
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. 
+
 use crate::{
     lexer::{self, Identifier},
     model::{Attribute, Class, Function, Type, View},
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+// Copyright (c) 2023, Oskar Ohlenmacher
+// All rights reserved
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
 use clap::Parser;
 use log::error;
 
