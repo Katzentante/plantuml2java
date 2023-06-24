6f4ce5be1ccba2c50688396317da3560eaf8d821
Oskar Ohlenmacher <katzentante@protonmail.com>
2023-06-24 15:03:59 +0200

    Organized generate, now working on new generate_classes func

--- src/generate.rs
+++ src/generate.rs
@@ -55,9 +55,54 @@ pub fn generate_files(
     Ok(())
 }
 
+fn write_class<'a>(class: &Class<'a>, location: &Path) -> Result<(), std::io::Error> {
+    let classpath = Path::new(class.name).with_extension("java");
+    let path = Path::join(location, classpath);
+    let mut file = File::create(&path)?;
+    file.write_all(class.to_java().as_bytes())?;
+    info!("successfully wrote to {}", path.display());
+    Ok(())
+}
+
+#[derive(Debug)]
+enum GeneratorError {
+    UnexpectedIdentifier(String),
+}
+
+impl std::fmt::Display for GeneratorError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::UnexpectedIdentifier(str) => write!(f, "{}", str),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum CustomError {
+    Utf8ParseError,
+    OutputNotDirectory,
+    InputNotFile,
+    InputNotFound,
+    // InputWrongExtension
+}
+
+impl std::fmt::Display for CustomError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Utf8ParseError => write!(f, "There is a utf8 error"),
+            Self::InputNotFile => write!(f, "Input file is a directory"),
+            Self::InputNotFound => write!(f, "Input file not present"),
+            Self::OutputNotDirectory => write!(f, "Output directory is a file"),
+            // _ => write!(f, "Some Error"),
+        }
+    }
+}
+
+impl Error for CustomError {}
+
 // TODO:
 // wait for start/enduml
-fn get_classes<'a>(idents: &'a [Token]) -> Result<Vec<Class<'a>>, GeneratorError> {
+/* fn get_classes<'a>(idents: &'a [Token]) -> Result<Vec<Class<'a>>, GeneratorError> {
     debug!("Converting identifiers: {:?}", idents);
     let mut classes = Vec::new();
     let mut is_abstract = false;
@@ -289,49 +334,5 @@ fn gen_method<'a>(
             is_static,
         ),
     ))
-}
+} */
 
-fn write_class<'a>(class: &Class<'a>, location: &Path) -> Result<(), std::io::Error> {
-    let classpath = Path::new(class.name).with_extension("java");
-    let path = Path::join(location, classpath);
-    let mut file = File::create(&path)?;
-    file.write_all(class.to_java().as_bytes())?;
-    info!("successfully wrote to {}", path.display());
-    Ok(())
-}
-
-#[derive(Debug)]
-enum GeneratorError {
-    UnexpectedIdentifier(String),
-}
-
-impl std::fmt::Display for GeneratorError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::UnexpectedIdentifier(str) => write!(f, "{}", str),
-        }
-    }
-}
-
-#[derive(Debug)]
-enum CustomError {
-    Utf8ParseError,
-    OutputNotDirectory,
-    InputNotFile,
-    InputNotFound,
-    // InputWrongExtension
-}
-
-impl std::fmt::Display for CustomError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Utf8ParseError => write!(f, "There is a utf8 error"),
-            Self::InputNotFile => write!(f, "Input file is a directory"),
-            Self::InputNotFound => write!(f, "Input file not present"),
-            Self::OutputNotDirectory => write!(f, "Output directory is a file"),
-            // _ => write!(f, "Some Error"),
-        }
-    }
-}
-
-impl Error for CustomError {}
